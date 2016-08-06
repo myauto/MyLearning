12761eaf175afaf26b95a15260228ce474a17bd6
nanda <sasi@theappbusiness.com>
2016-08-06 18:00:55 +0100

    Modified

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,7 +2,7 @@ package main.java.hello;
 
 public class Greeter {
     public String sayHello() {
-    	System.out.println("Inside sayHello method");
+    	System.out.println("Inside sayHello method - modified");
         return "Hello world!";
     }
 }
